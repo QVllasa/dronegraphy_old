8396c1ae35881534ea51631096954e7c8adef052
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2020-11-20 01:46:06 +0100

    working on frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,12 +4,10 @@ import { Settings } from 'luxon';
 import { DOCUMENT } from '@angular/common';
 import { Platform } from '@angular/cdk/platform';
 import { NavigationService } from '../@vex/services/navigation.service';
-import icLayers from '@iconify/icons-ic/twotone-layers';
 import { LayoutService } from '../@vex/services/layout.service';
 import { ActivatedRoute } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { SplashScreenService } from '../@vex/services/splash-screen.service';
 import { Style, StyleService } from '../@vex/services/style.service';
 import { ConfigName } from '../@vex/interfaces/config-name.model';
 
@@ -29,8 +27,7 @@ export class AppComponent {
               @Inject(LOCALE_ID) private localeId: string,
               private layoutService: LayoutService,
               private route: ActivatedRoute,
-              private navigationService: NavigationService,
-              private splashScreenService: SplashScreenService) {
+              private navigationService: NavigationService) {
     Settings.defaultLocale = this.localeId;
 
     if (this.platform.BLINK) {
