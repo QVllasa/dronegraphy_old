9fe6814220bb3d21fbffb634f7446d9b5e5bdd8a
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2020-12-16 01:04:21 +0100

    working on frontend

--- frontend/src/@dg/pipes/seconds/seconds-to-minutes.module.ts
+++ frontend/src/@dg/pipes/seconds/seconds-to-minutes.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { SecondsPipe} from './seconds-to-minutes.pipe';
+import { SecondsToMinutesPipe} from './seconds-to-minutes.pipe';
 
 
 @NgModule({
-  declarations: [SecondsPipe],
+  declarations: [SecondsToMinutesPipe],
   imports: [
     CommonModule
   ],
-  exports: [SecondsPipe]
+  exports: [SecondsToMinutesPipe]
 })
 export class SecondsToMinutesModule {
 }
